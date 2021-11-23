694c385679ee0e269ef6391e28254590b048a17e
Lance Hebert <LSUHEBERT@gmail.com>
2021-11-22 22:06:57 -0800

    commit tree 301c35c4cae1cf4ff4d5aa61b2e3fa1380ead573

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+
 // Code your solution in this file!
+function distanceFromHqInBlocks(input)
+{
+    let blocks = 0;
+    if (input >= 42)
+    {
+    blocks = input - 42; 
+    return blocks;  
+    }
+    else
+    {
+    const blocks = 42 - input;
+    return blocks;
+    }          
+}
+
+function distanceFromHqInFeet(input)
+{
+   const blockFeet = distanceFromHqInBlocks(input) * 264;
+   return blockFeet;
+}
+
+function distanceTravelledInFeet(north,south)
+{
+    const travelled = Math.abs(north - south) * 264;
+    return travelled;
+}
+
+function calculatesFarePrice(north, south)
+{
+    let fare = 0;
+    const distance = distanceTravelledInFeet(north,south);
+    if(distance > 2500)
+    {
+        return "cannot travel that far";
+    }
+    else
+    {
+        if(distance < 400)
+        {
+           return fare = 0;
+        }
+        else if ((distance - 400) > 0 && distance < 2000)
+        {
+           return fare = (distance - 400) * 0.02; 
+        }
+        else
+        {
+           return fare = 25;
+        }
+    }    
+}
